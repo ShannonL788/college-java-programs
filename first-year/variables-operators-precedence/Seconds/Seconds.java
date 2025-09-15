c1843fa77ca67cada1bb90d777073d915b45eee2
ShannonL788 <160933684+ShannonL788@users.noreply.github.com>
2025-09-15 17:00:00 +0100

    Create Seconds.java

---
+++ first-year/variables-operators-precedence/Seconds/Seconds.java
@@ -0,0 +1,7 @@
+public class Seconds {
+    public static void main(String [] args) {
+        int seconds = 365 * 24 * 60 * 60;
+      
+        System.out.println("The number of seconds in a year is " + seconds);
+    }
+}
