526ebf67559478d0e36568412e1f045a50f1310d
ShannonL788 <160933684+ShannonL788@users.noreply.github.com>
2025-09-15 12:48:39 +0100

    Create break4.java

---
+++ firstyear/variables-operators-precedence/break4/break4.java
@@ -0,0 +1,15 @@
+public class break4 {
+  public static void main(String [] args){
+    int num = 2016;
+
+    int digit1 = num / 1000;
+    int digit2 = (num / 100) % 10;
+    int digit3 = (num / 10) % 10;
+    int digit4 = num % 10;
+
+    System.out.println("The first digit is " + digit1);
+    System.out.println("The second digit is " + digit2);
+    System.out.println("The third digit is " + digit3);
+    System.out.println("The fourth digit is " + digit4);
+  }
+}
