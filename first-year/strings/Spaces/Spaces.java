f528f1c0be90f0c3643b8f17d265af5b38a6be2c
ShannonL788 <160933684+ShannonL788@users.noreply.github.com>
2025-09-16 11:17:20 +0100

    Create Spaces.java

---
+++ first-year/strings/Spaces/Spaces.java
@@ -0,0 +1,13 @@
+public class Spaces{
+    public static void main(String [] args){
+        String s1 = "notebook";
+        String s2 = "";
+        for(int i = 0; i < s1.length(); i++){
+            char ch = s1.charAt(i);
+            if(ch != ' '){
+                s2 += ch + " ";
+            }
+        }
+        System.out.println(s2);
+    }
+}
