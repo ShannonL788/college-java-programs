52ce3d224ebbbff993196408164db1de282a01bc
ShannonL788 <160933684+ShannonL788@users.noreply.github.com>
2025-09-16 11:10:51 +0100

    Create Sevens.java

---
+++ first-year/iteration/Sevens/Sevens.java
@@ -0,0 +1,11 @@
+public class Sevens{
+    public static void main(String [] args){
+        int i = 10;
+        while(i <= 70){
+            if(i % 7 == 0){
+                System.out.println(i + " ");
+            }
+            i++;
+        }
+    }
+}
